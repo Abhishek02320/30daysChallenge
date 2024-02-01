80a02b82e1b69c1a1c4324da9f8e3ae41e1cbab0
Abhishek02320 <113132228+Abhishek02320@users.noreply.github.com>
2024-02-02 01:01:53 +0530

    done

---
+++ day1/Day-1.js
@@ -0,0 +1,25 @@
+// const fs = require("fs");
+
+// function readFileContent(filePath) {
+//     fs.readFile(filePath, 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(`Error reading file: ${err.code} - ${err.message}`);
+//         } else {
+//             console.log(`File Content:\n${data}`);
+//         }
+//     });
+// }
+// Test Cases
+// 1st case
+// readFileContent('test-files/file1.txt');
+// 2nd case
+// readFileContent('test-files/empty-file.txt');
+// 3rd case
+// readFileContent('test-files/nonexistent-file.txt');
+
+
+
+
+
+
+
